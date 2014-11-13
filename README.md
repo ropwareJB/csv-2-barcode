13230d28b37810fb864464e811606c818886226f
Josh Brown <josh@epidev.com>
2014-11-13 22:54:29 +1100

    skeleton

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Python Barcode Generator
+
+Requires
+1. Python Package Installer - https://pip.pypa.io/en/latest/installing.html#install-pip
+2. Python Imaging Library ` pip install PIL --allow-external PIL --allow-unverified PI`
+3. PyPDF - https://github.com/reingart/pyfpdf
+
--- genBarcodes.py
+++ genBarcodes.py
@@ -1,7 +1,7 @@
 # Barcode Generator Tool
 # Josh Brown, 2014 EPIDEV
 #
-# Python 2.7 
+# Python 3.4 
 
 from fpdf import FPDF
 import csv
@@ -10,19 +10,25 @@ INPUT_FILE = "data.csv"
 
 class product:
     def __init__(self):
-        self.
+        pass
 
 
-
-with open(INPUT_FILE, newline='') as f:
-    reader = csv.reader(f)
+columns = False
+rows = []
+with open(INPUT_FILE, 'rU') as f:
+    reader = csv.reader(f, dialect=csv.excel_tab)
     for row in reader:
-        print(row)
+        if columns == False: 
+            columns = row
+            continue
+        rows.append(row)
 
-#pdf = FPDF()
-#pdf.add_page()
-#pdf.set_font('Arial','B',16)
-#pdf.cell(40,10,'Hello World!')
-#pdf.output('tuto1.pdf','F')
+print rows
 
-#$pdf->Image('http://chart.googleapis.com/chart?cht=p3&chd=t:60,40&chs=250x100&chl=Hello|World',60,30,90,0,'PNG');
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('Arial','B',16)
+pdf.cell(40,10,'Hello World!')
+
+pdf.image("http://barcode.tec-it.com/barcode.ashx?code=Code128&modulewidth=fit&data=ABC-abc-1234&dpi=96&imagetype=png&rotation=0&color=&bgcolor=&fontcolor=&quiet=0&qunit=mm", x=0, y=0, type='png', link='')
+pdf.output('tuto1.pdf','F')
