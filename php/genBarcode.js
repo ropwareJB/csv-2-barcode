3eab3b483006cd427a480aeafa4c04727c00d990
Josh Brown <josh@epidev.com>
2014-11-21 13:29:35 +1100

    Progress transition

--- php/genBarcode.js
+++ php/genBarcode.js
@@ -1,25 +1,39 @@
+var PROG = {current:0, real:0}
+
 function clickGenerate(){
+    $("#generateBtn").fadeOut(400, function(){ $("#progress").fadeIn(500);});
     $.get("/genBarcodes.php", null, genComplete).fail(genFail);
     //Start receiving progress
     setTimeout(function() {
       getProgress();
     }, 300);
+    updateProgressFrame();
 }
 function getProgress(){
     $.get("/getProgress.php", recProgressUpdate, "json").fail(recProgressUpdateFail);
 }
 function genComplete(result){
-  alert("Finished!");
   console.log(result);
 }
 function genFail(result){
   alert("Failed!!");
 }
 function recProgressUpdate(result){
-    console.log(result);
     var val = result.progress;
+    PROG.real = val;
     $("#progress").attr("value", val);
+    
     if(val<100) getProgress();
+}
+function updateProgressFrame(){
+    PROG.current += (PROG.real - PROG.current)/2;
+    if(PROG.real == 100 && PROG.real - PROG.current < 1) PROG.current = 100;
+    $("#percent").html(Math.floor(PROG.current)+"%");
+    if(PROG.current < 100){
+        setTimeout(function() {
+          updateProgressFrame();
+        }, 50);
+    }
 }
 function recProgressUpdateFail(result){
 
